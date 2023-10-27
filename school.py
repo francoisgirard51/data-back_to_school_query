28da8c51484668946eb3d6828447f23284565664
francois GIRARD <francoisgirard51@gmail.com>
2023-10-27 14:34:45 +0200

    students fetched by SQL

--- school.py
+++ school.py
@@ -1,15 +1,15 @@
+""" This return a list of students from a specific city
+"""
 # pylint:disable=C0111,C0103
 
-def students_from_city(db, city):
+import sqlite3
+
+conn = sqlite3.connect('data/school.sqlite')
+cursor = conn.cursor()
+
+def students_from_city(db_cursor, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+    db_cursor.execute("SELECT first_name, last_name FROM students WHERE birth_city = ?", (city,))
+    return [f"{row[0]} {row[1]}" for row in db_cursor.fetchall()]
 
-
-# To test your code, you can **run it** before running `make`
-#   => Uncomment the following lines + run:
-#   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+print(students_from_city(cursor, 'Paris'))
